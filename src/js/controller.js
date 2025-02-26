b5cfd40722215d2111f50c043702a636783cb9c5
eliasguillermo <eliasactisgrosso@gmail.com>
2025-02-26 20:08:14 -0300

    Test

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,7 +16,7 @@ if (module.hot) {
   module.hot.accept();
 }
 
-///////////////////////////////////////
+///////////////////////////////////
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
